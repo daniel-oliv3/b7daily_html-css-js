f829d12cfe332f99896b688ab37e4c1dfa9ae761
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-02-15 15:29:32 -0300

    Update

---
+++ Javascript-2/js/script.js
@@ -0,0 +1,13 @@
+//string = texto, sempre entre ASPAS  '' ou, ""
+//number ou interger ou float = número
+
+let nome = "Sapup3";
+let idade = 36;
+
+//Operação Adição
+let num1 = 10;
+let num2 = 5;
+
+let total = num1 + num2;
+
+console.log(total);
