5a3fae589e552a24319b270890c788492e7eb70c
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-02-15 15:50:03 -0300

    Condicional if else

--- Javascript-2/js/script.js
+++ Javascript-2/js/script.js
@@ -1,13 +1,40 @@
 //string = texto, sempre entre ASPAS  '' ou, ""
 //number ou interger ou float = número
 
+//* Variaveis
 let nome = "Sapup3";
 let idade = 36;
 
-//Operação Adição
 let num1 = 10;
 let num2 = 5;
 
-let total = num1 + num2;
+//Operação Adição
+let total1 = num1 + num2;
 
-console.log(total);
+//Operação Subtração
+let total2 = num1 - num2;
+
+//Operação Multiplicação
+let total3 = num1 * num2;
+
+//Operação Divisão
+let total4 = num1 / num2;
+
+//Resultado no console
+console.log();
+
+
+//* Condicional IF ELSE
+let  total = 32;
+
+console.log("O total foi: " + total);
+
+if(total > 50){
+    console.log("Total foi MAIOR que 50");
+}else{
+    console.log("Total foi Menor que 50");
+}
+
+
+
+//* Laços de repetição
